b66eefc8c5c5c28561f6ef01c35be73d87bf04c3
Gabriel <gabriel.zubairu@academytrainee.com>
2020-04-17 14:19:56 +0100

    TDD 2

---
+++ src/main/java/com/qa/testdrivendevelopment/Demo.java
@@ -0,0 +1,15 @@
+package com.qa.testdrivendevelopment;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		Questions q = new Questions();
+		
+		
+		
+		System.out.println(q.isPrime(3));
+		
+	}
+
+}
